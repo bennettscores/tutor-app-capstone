30d78e2131d78084f2f9a91cad02e1f485fdaffd
bennettscores <bennettscoresby1234@gmail.com>
2019-11-01 14:00:46 -0600

    commented out validation for a test

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,11 @@ const User = require("../models/user");
 
 router.post("/register", (req, res) => {
 
-    const { errors, isValid } = validateRegisterInput(req.body)
+    // const { errors, isValid } = validateRegisterInput(req.body)
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
+    // if (!isValid) {
+    //     return res.status(400).json(errors);
+    // }
 
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
